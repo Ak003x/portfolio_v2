99dfdb317526d544cd87f6fc05fb1373849a2494
Dev_Ak <aakash.velu.003@gmail.com>
2026-02-04 03:41:10 +0530

    grid fixed

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -18,17 +18,16 @@ const Hero = () => {
       </div>
 
       <div>
-        <div className="relative flex h-screen w-full items-center justify-center bg-white dark:bg-black-100 dark:bg-grid-white/[0.3]">
-          <div
-            className={cn(
-              "absolute inset-0",
-              "[background-size:20px_20px]",
-              "[background-image:radial-gradient(#d4d4d4_1px,transparent_1px)]",
-              "dark:[background-image:radial-gradient(#404040_1px,transparent_1px)]",
-            )}
-          />
-
+        <div className="absolute bg-white  top-0 left-0 flex h-screen w-full items-center  justify-center  dark:bg-black-100 dark:bg-grid-white/[0.03]  ">
           <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black-100"></div>
+        </div>
+      </div>
+
+      <div className="flex justify-center relative my-20 z-10">
+        <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
+          <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80 ">
+            Dynamic web Magic with Next.js
+          </h2>
         </div>
       </div>
     </div>
