7e3ca376f21db6ee36bd4b6f26cf5199823cb3c6
Dev_Ak <aakash.velu.003@gmail.com>
2026-02-05 01:18:53 +0530

    refine

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -1,11 +1,23 @@
 import { title } from 'process';
 import React from 'react';
+import { BentoGrid, BentoGridItem } from './ui/BentoGrid';
+import { i } from 'motion/react-client';
 
 const Grid = () => {
-  return <section>
-
-    
-  </section>;
+  return (
+    <section id="about">
+      <BentoGrid>
+        {[{ title: 'Title1', description: 'Desc1', id: 1 }].map((item) => (
+          <BentoGridItem
+            id={item.id}
+            key={item.id}
+            title={item.title}
+            description={item.description}
+          />
+        ))}
+      </BentoGrid>
+    </section>
+  );
 };
 
 export default Grid;
--- src/components/ui/BentoGrid.tsx
+++ src/components/ui/BentoGrid.tsx
@@ -1,4 +1,4 @@
-import { cn } from "@/lib/utils";
+import { cn } from '@/lib/utils';
 
 export const BentoGrid = ({
   className,
@@ -10,8 +10,8 @@ export const BentoGrid = ({
   return (
     <div
       className={cn(
-        "mx-auto grid max-w-7xl grid-cols-1 gap-4 md:auto-rows-[18rem] md:grid-cols-3",
-        className,
+        'mx-auto grid max-w-7xl grid-cols-1 gap-4 md:auto-rows-[18rem] md:grid-cols-3',
+        className
       )}
     >
       {children}
@@ -25,18 +25,20 @@ export const BentoGridItem = ({
   description,
   header,
   icon,
+  id,
 }: {
   className?: string;
   title?: string | React.ReactNode;
   description?: string | React.ReactNode;
   header?: React.ReactNode;
   icon?: React.ReactNode;
+  id: number;
 }) => {
   return (
     <div
       className={cn(
-        "group/bento shadow-input row-span-1 flex flex-col justify-between space-y-4 rounded-xl border border-neutral-200 bg-white p-4 transition duration-200 hover:shadow-xl dark:border-white/[0.2] dark:bg-black dark:shadow-none",
-        className,
+        'row-span-1 relative rounded-3xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border border-transparent justify-between flex flex-col space-y-4 border-red-500',
+        className
       )}
     >
       {header}
