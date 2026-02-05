f3de085d237328c86fe3690ca77b67cdda8a01cd
Dev_Ak <aakash.velu.003@gmail.com>
2026-02-06 01:58:31 +0530

    will continue..

--- src/components/ui/BentoGrid.tsx
+++ src/components/ui/BentoGrid.tsx
@@ -23,14 +23,22 @@ export const BentoGridItem = ({
   className,
   title,
   description,
-  header,
-  icon,
+  id,
+  img,
+  imgClassName,
+  titleClassName,
+  spareImg,
 }: {
   className?: string;
   title?: string | React.ReactNode;
   description?: string | React.ReactNode;
   header?: React.ReactNode;
   icon?: React.ReactNode;
+  id?: number;
+  img?: string;
+  imgClassName?: string;
+  titleClassName?: string;
+  spareImg?: string;
 }) => {
   return (
     <div
@@ -38,10 +46,16 @@ export const BentoGridItem = ({
         'row-span-1 relative rounded-3xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border border-transparent justify-between flex flex-col space-y-4 ',
         className
       )}
+      style={{
+        background: 'rgb(4,7,29)',
+        backgroundColor:
+          'linear-gradient(90deg, rgba(4,7,29,1) 0%, rgba(12,14,35,1) 100%)',
+      }}
     >
-      {header}
+
+<div className=''></div>
+
       <div className="transition duration-200 group-hover/bento:translate-x-2">
-        {icon}
         <div className="mt-2 mb-2 font-sans font-bold text-neutral-600 dark:text-neutral-200">
           {title}
         </div>
