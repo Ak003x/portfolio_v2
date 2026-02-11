d804682449f84911b3e70dabbb5032f3e56f5d9c
Dev_Ak <aakash.velu.003@gmail.com>
2026-02-12 00:28:22 +0530

    save

--- src/components/RecentProject.tsx
+++ src/components/RecentProject.tsx
@@ -19,8 +19,8 @@ const RecentProject = () => {
               className="lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:w-96 w-[80vw]"
             >
               <PinContainer title={title} href={link}>
-                <div className="relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[20vh] lg:h-[30vh]">
-                  <div>
+                <div className="relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[20vh] lg:h-[30vh] mb-10">
+                  <div className="relative w-full h-full overflow-hidden lg:rounded-3xl bg-[#13162d]">
                     <img src="/bg.png" alt="bg-img" />
                   </div>
                   {/* // projects img */}
@@ -29,7 +29,14 @@ const RecentProject = () => {
                     alt={title}
                     className="z-10 absolute bottom-0"
                   />
+
                 </div>
+
+                <h1 className="font-bold lg:text-2xl md:text-xl text-base line-clamp-1">
+                  {title}
+                </h1>
+
+                <p className=''>{des} </p>
               </PinContainer>
             </div>
           );
