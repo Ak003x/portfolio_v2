12604745e8c703f2b53bdb38aada93140b0a7132
Dev_Ak <aakash.velu.003@gmail.com>
2026-02-12 23:04:20 +0530

    all save

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,8 +15,8 @@ export default function Home() {
           navItems={[{ name: 'Home', link: '/', icon: <FaHome /> }]}
         />
         <Hero />
-        <Grid/>
-        <RecentProject/>
+        <Grid />
+        <RecentProject />
       </div>
     </main>
   );
