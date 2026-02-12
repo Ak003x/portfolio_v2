4b5a40b885064b80a0d265a68a97b4fdc042d415
Dev_Ak <aakash.velu.003@gmail.com>
2026-02-12 21:26:20 +0530

    project section

--- src/components/RecentProject.tsx
+++ src/components/RecentProject.tsx
@@ -1,6 +1,7 @@
 import { projects } from '@/data/index';
 import React from 'react';
 import { PinContainer } from './ui/3d-pin';
+import { FaLocationArrow } from 'react-icons/fa6';
 
 const RecentProject = () => {
   return (
@@ -29,14 +30,37 @@ const RecentProject = () => {
                     alt={title}
                     className="z-10 absolute bottom-0"
                   />
-
                 </div>
 
                 <h1 className="font-bold lg:text-2xl md:text-xl text-base line-clamp-1">
                   {title}
                 </h1>
 
-                <p className=''>{des} </p>
+                <p className="lg:text-xl lg:font-normal font-light text-sm line-clamp-2">
+                  {des}{' '}
+                </p>
+
+                <div className="flex items-center justify-between mt-7 mb-3">
+                  <div className="flex items-center">
+                    {iconLists.map((icon, index) => (
+                      <div
+                        key={icon}
+                        className="border border-white/[0.2] rounded-full bg-black lg:w-10 lg:h-10 w-8 h-8 flex justify-center 
+                        items-center "
+                        style={{ transform: `translateX(-${5 * index * 2}px)` }}
+                      >
+                        <img src={icon} alt={icon} className="p-2" />
+                      </div>
+                    ))}
+                  </div>
+
+                  <div className="flex justify-center items-center">
+                    <p className="flex lg:text-xl md:text-xs text-sm text-purple">
+                      Check live site
+                    </p>
+                    <FaLocationArrow className="ms-3" color="#CBACF" />
+                  </div>
+                </div>
               </PinContainer>
             </div>
           );
