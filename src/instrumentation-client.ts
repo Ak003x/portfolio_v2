13fc586abe49bd9355129d66df3174bdd3d2d8a3
Akash <aakash.velu.003@gmail.com>
2026-02-13 22:00:38 +0530

    report bug sentry

--- src/instrumentation-client.ts
+++ src/instrumentation-client.ts
@@ -8,7 +8,13 @@ Sentry.init({
   dsn: 'https://d0f5cdf4f334c2c0c2816da69b8ef151@o4510879389581312.ingest.us.sentry.io/4510879400853504',
 
   // Add optional integrations for additional features
-  integrations: [Sentry.replayIntegration()],
+  integrations: [
+    Sentry.replayIntegration(),
+    Sentry.feedbackIntegration({
+      // Additional SDK configuration goes in here, for example:
+      colorScheme: 'system',
+    }),
+  ],
 
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
   tracesSampleRate: 1,
