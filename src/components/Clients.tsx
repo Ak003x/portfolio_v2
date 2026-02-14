a9845052d93dcbc52a918032a9e15af9b27e5302
Akash <aakash.velu.003@gmail.com>
2026-02-15 00:21:55 +0530

    refine

--- src/components/Clients.tsx
+++ src/components/Clients.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { InfiniteMovingCards } from './ui/infinteMovingCards';
+import { testimonials } from '@/data/index';
 
 const clients = () => {
   return (
@@ -8,7 +10,15 @@ const clients = () => {
         <span className="text-purple"> satisfied clients </span>
       </h1>
 
-      <div className="flex flex-col items-center justify-center "></div>
+      <div className="flex flex-col items-center justify-center ">
+        <div className="h-[50vh] md:h-[30rem] rounded-md flex flex-col antialiased items-center relative overflow-hidden">
+          <InfiniteMovingCards
+            items={testimonials}
+            direction="right"
+            speed="slow"
+          />
+        </div>
+      </div>
     </div>
   );
 };
