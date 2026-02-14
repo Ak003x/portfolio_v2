9ccd602509f03d9ada10f3b11eeb5b8485afd9af
Akash <aakash.velu.003@gmail.com>
2026-02-15 00:57:11 +0530

    Next section experience

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import RecentProject from '@/components/RecentProject';
 import { FloatingNav } from '@/components/ui/FloatingNav';
 import { navItems } from '@/data/index';
 import { FaHome } from 'react-icons/fa';
+import Experience from '@/components/Experience';
 
 export default function Home() {
   return (
@@ -18,6 +19,7 @@ export default function Home() {
         <Grid />
         <RecentProject />
         <Clients />
+        <Experience/>
       </div>
     </main>
   );
