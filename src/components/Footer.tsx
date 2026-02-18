51b42948385fb2ad57f36ab0d9e3fa10451ab251
Akash <aakash.velu.003@gmail.com>
2026-02-18 23:36:20 +0530

    contact done

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -36,11 +36,11 @@ const Footer = () => {
         <p className="md:text-base  md:font-normal font-light">
           Copyright © 2026 Akash
         </p>
-        <div className="flex items-center md:gap-3 gap-6">
+        <div className="flex items-center md:gap-3 gap-6 py-2">
           {socialMedia.map((profile) => (
             <div
               key={profile.id}
-              className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200"
+              className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
             >
               <img src={profile.img} alt={profile.id} width={20} height={20} />
             </div>
