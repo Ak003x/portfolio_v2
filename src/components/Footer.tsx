eb0e9a4bd832f5bb3481229196780cb75fc13fa8
Akash <aakash.velu.003@gmail.com>
2026-02-20 11:23:27 +0530

    Fixed the error

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import Image from 'next/image';
 import MagicButton from './ui/MagicButton';
 import { FaLocationArrow } from 'react-icons/fa6';
 import { socialMedia } from '@/data/index';
 
 const Footer = () => {
   return (
-    <footer className="w-full mb-[100px] pb-10 md:mb-5" id="contact">
+    <footer className="w-full mb-[10px] pb-10 md:mb-5" id="contact">
       <div className="flex flex-col items-center">
         <h1 className="heading lg:max-w-[45vw]">
           Ready to take <span className="text-purple">Your</span>
@@ -32,12 +33,17 @@ const Footer = () => {
           {socialMedia.map((profile) => (
             <a
               key={profile.id}
-              href={profile.link} 
-              target="_blank" 
+              href={profile.link}
+              target="_blank"
               rel="noopener noreferrer"
               className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
             >
-              <img src={profile.img} alt={profile.id} width={20} height={20} />
+              <Image
+                src={profile.img}
+                alt={String(profile.id)}
+                width={20}
+                height={20}
+              />
             </a>
           ))}
         </div>
--- src/components/ui/BentoGrid.tsx
+++ src/components/ui/BentoGrid.tsx
@@ -113,7 +113,6 @@ export const BentoGridItem = ({
             'group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10'
           )}
         >
-          {/* change the order of the title and des, font-extralight, remove text-xs text-neutral-600 dark:text-neutral-300 , change the text-color */}
           <div className="font-sans font-extralight md:max-w-32 md:text-xs lg:text-base text-sm text=white z-10">
             {description}
           </div>
