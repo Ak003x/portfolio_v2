84632af0bc0506c4a21391729ccbfc4b285e8bad
Akash <aakash.velu.003@gmail.com>
2026-02-23 17:54:05 +0530

    updated the projects pics

--- data/index.ts
+++ data/index.ts
@@ -75,8 +75,8 @@ export const gridItems = [
 export const projects = [
   {
     id: 1,
-    title: '3D Solar System Planets to Explore',
-    des: 'Explore the wonders of our solar system with this captivating 3D simulation of the planets using Three.js.',
+    title: 'GFL - Global Foundation for Learning',
+    des: 'A non-profit educational platform empowering students worldwide with essential skills in communication, critical thinking, and comprehensive learning development.',
     img: '/p1.svg',
     iconLists: ['/re.svg', '/tail.svg', '/ts.svg', '/three.svg', '/fm.svg'],
     link: '/ui.earth.com',
